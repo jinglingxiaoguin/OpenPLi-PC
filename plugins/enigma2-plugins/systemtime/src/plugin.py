5fb3eda2f4bf5c8eb8d730bc0c92303bcababb39
ag1455 <a.g.prosat@gmail.com>
2022-10-09 17:36:55 +0300

    Fix plugin systemtime.

--- plugins/enigma2-plugins/systemtime/src/plugin.py
+++ plugins/enigma2-plugins/systemtime/src/plugin.py
@@ -16,7 +16,7 @@ from Screens.MessageBox import MessageBox
 from Screens.Standby import TryQuitMainloop
 from Screens.Screen import Screen
 from Tools.Directories import fileExists
-from NTPSyncPoller import NTPSyncPoller
+from .NTPSyncPoller import NTPSyncPoller
 import os
 import time
 from calendar import isleap
