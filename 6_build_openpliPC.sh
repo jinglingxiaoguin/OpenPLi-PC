c215c6f8238ee9b6c51f4c790c8dd69ba213c197
ag1455 <a.g.prosat@gmail.com>
2022-10-15 02:24:14 +0300

    Update enigma2 *.po.

--- 6_build_openpliPC.sh
+++ 6_build_openpliPC.sh
@@ -137,7 +137,7 @@ rpl "//#define XINE_TEXTDOMAIN" "#define XINE_TEXTDOMAIN" /usr/include/xine/xine
 
 git clone https://github.com/OpenPLi/$PKG.git
 cd $PKG
-git reset --hard fbf69f9b
+git reset --hard 33424287
 cd ..
 
 # Copy headers
@@ -164,9 +164,9 @@ if [ "$release" = "22.04" ]; then
 	fi
 fi
 
-cp -fv patches/patch-fbf69f9b-to-PC.patch $PKG
+cp -fv patches/patch-33424287-to-PC.patch $PKG
 cd $PKG
-patch -p1 < patch-fbf69f9b-to-PC.patch
+patch -p1 < patch-33424287-to-PC.patch
 
 # Configure
 if [ "$DO_CONFIGURE" -eq "1" ]; then
