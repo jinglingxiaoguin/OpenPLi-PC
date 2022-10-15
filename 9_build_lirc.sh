0572712950dfa9294a149e77ebcb9992f3503044
ag1455 <a.g.prosat@gmail.com>
2022-10-15 20:41:23 +0300

    Fix build lirc with python-3.10. Remove unusable patches.

--- 9_build_lirc.sh
+++ 9_build_lirc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script for install patched lirc-0.10.1. Get repeats again. In version Ubuntu-14.04, use lirc 0.9.0 from the repository only.
+# Script for install patched lirc-0.10.1. Get repeats again.
 
 # If your system has two devices /dev/lirc0 and /dev/lirc1 (for example, a built-in IR-receiver in the card card)
 # then you can add a rule to /etc/udev/rules.d/99-lirc-symlinks.rules:
@@ -18,13 +18,15 @@ release=$(lsb_release -a 2>/dev/null | grep -i release | awk ' { print $2 } ')
 dpkg -r liblirc-dev liblirc0 liblircclient-dev lirc lirc-doc lirc-x
 
 if [[ "$release" = "22.04" ]]; then
+	pip install --upgrade websockets
+	pip uninstall PyCrypto
+	pip install -U PyCryptodome
+
 	apt install -y dh-exec dh-python doxygen expect libftdi1-dev libsystemd-dev libudev-dev libusb-dev man2html-base portaudio19-dev python3-dev python3-setuptools socat setserial xsltproc
 	wget http://ftp.de.debian.org/debian/pool/main/d/debhelper/dh-systemd_13.2.1_all.deb
 	dpkg -i dh-systemd_13.2.1_all.deb
 	rm -f dh-systemd_13.2.1_all.deb
 	dpkg -i pre/lirc/*.deb
-else
-	apt install -y dh-exec dh-python dh-systemd doxygen expect libftdi1-dev libsystemd-dev libudev-dev libusb-dev man2html-base portaudio19-dev python3-dev python3-setuptools socat setserial xsltproc
 
 	mkdir $DIR
 	cd $DIR
@@ -35,13 +37,9 @@ else
 	tar -xvf lirc_0.10.1.orig.tar.gz
 	rm -vf lirc_0.10.1.orig.tar.gz
 	cd ..
-	cp -v patches/lirc_0.10.1-6.patch $DIR/$PKG
-	cp -v patches/python38_client_py.patch $DIR/$PKG
+	cp -v patches/lirc-python3.10.patch $DIR/$PKG
 	cd $DIR/$PKG
-	patch -p1 < lirc_0.10.1-6.patch
-	if [[ "$release" = "20.04" ]]; then
-		patch -p1 < python38_client_py.patch
-	fi
+	patch -p1 < lirc-python3.10.patch
 	rm -f *.patch
 	chmod 755 debian/install
 	chmod 755 debian/pbuilder-test
@@ -52,28 +50,23 @@ else
 	cd $PKG
 	dpkg-buildpackage -b -d -uc -us
 	cd ..
-
-	if [[ "$release" = "16.04" ]]; then
-		# Hack. We need to resolve dependencies.
-		echo "*** 16.04 ***"
 		dpkg -i *.deb
-		sleep 1
-		dpkg -i *.deb
-	else
-		dpkg -i *.deb
-	fi
-
 	cd ..
+else
+	echo "                                           "
+	echo "              NOT SUPPORT!                 "
+	echo "                                           "
 fi
 
 cp -rfv pre/lirc/lircd.conf.d /etc
 cp -rfv pre/lirc/lirc_options.conf.example /etc
 cp -fv pre/99-lirc-symlinks.rules /etc/udev/rules.d
+
 if [ -f $CONF/lircd.conf.d/devinput.lircd.conf ]; then
-	mv $CONF/lircd.conf.d/devinput.lircd.conf /etc/lirc/lircd.conf.d/devinput.lircd.conf.dist
+	mv -b $CONF/lircd.conf.d/devinput.lircd.conf /etc/lirc/lircd.conf.d/devinput.lircd.conf.dist
 fi
 if [ -f $CONF/irexec.lircrc ]; then
-	mv $CONF/irexec.lircrc $CONF/irexec.lircrc.dist
+	mv - b$CONF/irexec.lircrc $CONF/irexec.lircrc.dist
 fi
 if [ -f $CONF/irexec.lircrc ];then
 	rm -f $CONF/irexec.lircrc
