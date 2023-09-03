fab7b1aeac44b4d7c68ee39e63ff26451932d59f
ag1455 <a.g.prosat@gmail.com>
2023-09-03 12:45:11 +0300

    Update enigma2, e2iplayer, libdvbsi++ repository. Add 4k skin only for vaapi OSD on 4K monitor.

--- 7_build_plugins.sh
+++ 7_build_plugins.sh
@@ -460,7 +460,7 @@ if [ "$release" = "22.04" ]; then
 			PKG="e2iplayer"
 			PKG_="IPTVPlayer"
 			PKG__="E2IPlayer"
-			VER="3a60481cfeb424a6288f46d3accad03365cbf31e"
+			VER="604b7442172bb6ca8d2e672a34bf1d759dada19a"
 			if [ -d $PKG ]; then
 				rm -rf $PKG
 			fi
