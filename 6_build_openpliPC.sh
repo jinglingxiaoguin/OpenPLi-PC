0aa95460a5f02a4eba97b0fdce965730bd89ec9a
ag1455 <a.g.prosat@gmail.com>
2023-10-03 00:07:45 +0300

    Update enigma2.

--- 6_build_openpliPC.sh
+++ 6_build_openpliPC.sh
@@ -155,7 +155,7 @@ rpl "//#define XINE_TEXTDOMAIN" "#define XINE_TEXTDOMAIN" /usr/include/xine/xine
 
 git clone https://github.com/OpenPLi/$PKG.git
 cd $PKG
-git reset --hard e071dcf6
+git reset --hard 52d1aeb2
 cd ..
 
 # Copy headers
@@ -167,9 +167,9 @@ if [[ ! -d /usr/include/netlink ]]; then
 	ln -s /usr/include/libnl3/netlink /usr/include
 fi
 
-cp -fv patches/patch-e071dcf6-to-PC.patch $PKG
+cp -fv patches/patch-52d1aeb2-to-PC.patch $PKG
 cd $PKG
-patch -p1 < patch-e071dcf6-to-PC.patch
+patch -p1 < patch-52d1aeb2-to-PC.patch
 
 # Configure
 if [[ "$DO_CONFIGURE" -eq "1" ]]; then
