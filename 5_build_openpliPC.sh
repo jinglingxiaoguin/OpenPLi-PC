29620f69dbf7fd00091c6f3aab40d4f33b257208
ag1455 <a.g.prosat@gmail.com>
2024-12-02 23:35:01 +0300

    Update enigma2. Important update: In the new environment, there were some annoying horizontal image breaks.

--- 5_build_openpliPC.sh
+++ 5_build_openpliPC.sh
@@ -146,7 +146,7 @@ rpl "//#define XINE_TEXTDOMAIN" "#define XINE_TEXTDOMAIN" /usr/include/xine/xine
 
 git clone https://github.com/OpenPLi/$PKG.git
 cd $PKG
-git reset --hard 69a69f4b
+git reset --hard f546a589
 cd ..
 
 # Create symlinks in /usr diectory before compile enigma2
@@ -154,9 +154,9 @@ if [[ ! -d /usr/include/netlink ]]; then
 	ln -s /usr/include/libnl3/netlink /usr/include
 fi
 
-cp -fv patches/patch-69a69f4b-to-PC.patch $PKG
+cp -fv patches/patch-f546a589-to-PC.patch $PKG
 cd $PKG
-patch -p1 < patch-69a69f4b-to-PC.patch
+patch -p1 < patch-f546a589-to-PC.patch
 
 # Configure
 if [[ "$DO_CONFIGURE" -eq "1" ]]; then
